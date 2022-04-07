2e013bafe2190978bca26b1cb000810465c303b2
JuanCampos1 <88661667+JuanCampos1@users.noreply.github.com>
2022-04-07 18:48:41 -0300

    1

---
+++ PII_2021_2_Object_Lifecycle-main/src/ClassLibrary/Train.cs
@@ -0,0 +1,85 @@
+﻿//--------------------------------------------------------------------------------
+// <copyright file="Train.cs" company="Universidad Católica del Uruguay">
+//     Copyright (c) Programación II. Derechos reservados.
+// </copyright>
+//--------------------------------------------------------------------------------
+
+using System;
+
+namespace ClassLibrary
+{
+    /// <summary>
+    /// Esta clase representa un tren muy básico.
+    /// </summary>
+    public class Train
+    {
+        private static int count = 0;
+
+        public static int counter
+        {
+            get
+            {
+                return count;
+            }
+            set
+            {
+                count = value;
+            }
+        }
+        public string train { get; set; }
+
+        public Train (string train)
+        {
+            this.train = train;
+            count++;
+        }
+
+        ~Train()
+        {
+            count--;
+        }
+        /// <summary>
+        /// Obtiene un valor que indica si las maquinas del tren han sido encendidas o no.
+        /// </summary>
+        /// <value><c>true</c> si las máquinas fueron encendidas, <c>false</c> en caso contrario.</value>
+        public bool IsEngineStarted { get; private set; }
+
+        /// <summary>
+        /// Enciende las máquinas del tren.
+        /// </summary>
+        /// <returns>
+        /// <c>true</c> si las máquinas pueden ser encendidas, <c>false</c> en caso contrario.
+        /// </returns>
+        public bool StartEngines()
+        {
+            if (this.IsEngineStarted)
+            {
+                Console.Write("The engines are already running");
+                return false;
+            }
+
+            this.IsEngineStarted = true;
+            Console.Write("Engines on");
+            return true;
+        }
+
+        /// <summary>
+        /// Detiene las máquinas del tren.
+        /// </summary>
+        /// <returns>
+        /// <c>true</c> si las máquinas pueden ser detenidas, <c>false</c> en caso contrario.
+        /// </returns>
+        public bool StopEngines()
+        {
+            if (this.IsEngineStarted)
+            {
+                this.IsEngineStarted = false;
+                Console.Write("Engines off");
+                return true;
+            }
+
+            Console.Write("The engines are already stopped");
+            return this.IsEngineStarted;
+        }
+    }
+}
